c876109c40141126af1203f06857a2bf7f0684dc
Luca Di Vincenzo <luca.divincenzo@piksel.com>
2019-07-05 15:20:40 +0200

    adding tests coverage for followNext scraper

--- test/followNextScraper.js
+++ test/followNextScraper.js
@@ -3,7 +3,7 @@ const Code = require('@hapi/code');
 const Axios = require('axios');
 const Sinon = require('sinon');
 const FS = require('fs');
-const Path = require('path')
+const Path = require('path');
 const { NotImplementedError } = require('../lib/errors');
 const Rattler = require('../');
 
@@ -26,27 +26,28 @@ describe('Rattler', () => {
       const baseURL = 'http://www.example.com';
       const searchURL = '/page-1';
       let axiosSpy;
+
       describe('(with no errors)', () => {
 
-        before(async () => {
-          axiosSpy = Sinon.stub(Axios, 'get').callsFake(async (url) => {
-            const page = url.split(`${baseURL}/`)[1];
-            const html = FS.readFileSync(Path.join(__dirname, `/artifacts/${page}.html`), 'utf8');
-            return {
-              data: html
-            };
+        describe('(when all pages exists)', () => {
+
+          before(async () => {
+            axiosSpy = Sinon.stub(Axios, 'get').callsFake(async (url) => {
+              const page = url.split(`${baseURL}/`)[1];
+              const html = FS.readFileSync(Path.join(__dirname, `/artifacts/${page}.html`), 'utf8');
+              return {
+                data: html
+              };
+            });
           });
-        });
 
-        after(async () => {
-          Axios.get.restore();
-        });
+          after(async () => {
+            Axios.get.restore();
+          });
 
-        afterEach(async () => {
-          axiosSpy.resetHistory();
-        });
-
-        describe('(followNext)', () => {
+          afterEach(async () => {
+            axiosSpy.resetHistory();
+          });
 
           it('should return the extracted text from all followed pages', async () => {
             const config = {
@@ -72,10 +73,103 @@ describe('Rattler', () => {
               expect(res.pages[i].extractedInfo).to.equal(`Page ${i + 1}`);
             }
           });
-
-          // TODO add test for selector not found
         });
 
+        // TODO add test for selector not found
+
+      });
+
+      describe('(with errors)', () => {
+
+        describe('(404 from one page)', () => {
+
+          before(async () => {
+            axiosSpy = Sinon.stub(Axios, 'get').callsFake(async (url) => {
+              const page = url.split(`${baseURL}/`)[1];
+              if (page === 'page-3') {
+                const err = new Error('Request failed with status code 404');
+                err.response = {
+                  status: 404,
+                  statusText: 'Not Found'
+                };
+                err.data = { statusCode: 404, error: 'Not Found', message: 'Not Found' };
+                throw err;
+              }
+              const html = FS.readFileSync(Path.join(__dirname, `/artifacts/${page}.html`), 'utf8');
+              return {
+                data: html
+              };
+            });
+          });
+
+          after(async () => {
+            Axios.get.restore();
+          });
+
+          afterEach(async () => {
+            axiosSpy.resetHistory();
+          });
+
+          it('should return the extracted text up to the page that failed', async () => {
+            const config = {
+              baseURL,
+              scrapeList: [{
+                label: 'pages',
+                searchURL,
+                cssSelector: 'h1',
+                followNext: {
+                  cssSelector: '#nextLink',
+                  maxDepth: 5
+                }
+              }]
+            };
+            const rt = new Rattler(config);
+            const res = await rt.extract();
+            expect(res.pages).to.exist();
+            // TODO brittle, if we add more pages this will fail
+            expect(res.pages).to.have.length(3);
+            expect(res.pages[0].extractedFrom).to.equal('http://www.example.com/page-1');
+            expect(res.pages[0].extractedWith).to.equal('h1');
+            expect(res.pages[0].extractedInfo).to.equal('Page 1');
+
+            expect(res.pages[1].extractedFrom).to.equal('http://www.example.com/page-2');
+            expect(res.pages[1].extractedWith).to.equal('h1');
+            expect(res.pages[1].extractedInfo).to.equal('Page 2');
+
+            expect(res.pages[2].extractedInfo).to.be.instanceof(Error);
+          });
+
+          it('should return the error in the extractedInfo of the page that failed', async () => {
+            const config = {
+              baseURL,
+              scrapeList: [{
+                label: 'pages',
+                searchURL,
+                cssSelector: 'h1',
+                followNext: {
+                  cssSelector: '#nextLink',
+                  maxDepth: 5
+                }
+              }]
+            };
+            const rt = new Rattler(config);
+            const res = await rt.extract();
+            expect(res.pages).to.exist();
+            // TODO brittle, if we add more pages this will fail
+            expect(res.pages).to.have.length(3);
+
+            expect(res.pages[2].extractedFrom).to.equal('http://www.example.com/page-3');
+            expect(res.pages[2].extractedWith).to.equal('h1');
+            expect(res.pages[2].extractedInfo).to.be.instanceof(Error);
+            expect(res.pages[2].extractedInfo.message).to.equal('Response returned out of range status code');
+            expect(res.pages[2].extractedInfo.statusCode).to.equal(404);
+            expect(res.pages[2].extractedInfo.reason).to.equal({
+              statusCode: 404,
+              error: 'Not Found',
+              message: 'Not Found'
+            });
+          });
+        });
       });
 
     });
